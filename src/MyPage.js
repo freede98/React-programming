ac50c596b523aa8b26197404d638cd176cde30be
freede98 <36030202+freede98@users.noreply.github.com>
2021-08-04 13:56:21 +0200

    Update MyPage.js

--- src/MyPage.js
+++ src/MyPage.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import './index.css';
-//import Checkbox from './components/Checkbox';
 //import Footer from './components/Footer';
 //import MainContent from './components/MainContent';
 //import Navbar from './components/Navbar';
 //import Joke from './components/Joke'
 //import Product from './components/Product'
 //import productsData from './components/productsData'
-//import LoggedIn from './components/LoggedIn'
-//import MouseOver from './components/MouseOver';
-//import Count from './components/Count';
-//import Conditional from './components/Conditional';
-//import LogInPractice from './components/LogInPractice'
-//import TodoList from './components/TodoList';
-//import Fetch from './components/Fetch'
-//import TravelForm from './components/TravelForm'
+
+/*
+    Import any componenets as shown above
+*/
+
 import MemePage from "./components/MemePage"
 
 function MyPage(){
-    /*const date = new Date()
-    const hours = date.getHours()
-    const minutes = date.getMinutes()*/
     /*return (
       <div>
         <Navbar />
@@ -30,30 +23,6 @@ function MyPage(){
         <Footer/>
       </div>
     )*/
-     /* return(
-        <div>
-        <Joke 
-          question="Why did the chicken cross the road?"
-          answer="She had to find the egg!"
-        />
-        <Joke 
-          question="What's up with airplane food?"
-          answer="They want you to jump off the plane to save fuel!"
-        />
-        <Joke 
-          //question="Why did the chicken cross the road?"
-          answer="Kleptomaniacs amiright?"
-        />
-        <Joke 
-          question="Why did the chicken cross the road?"
-          answer="She had to find the egg!"
-        />
-        <Joke 
-          question="Why did the chicken cross the road?"
-          answer="She had to find the egg!"
-        />
-        </div>
-      )*/
       /*const productComponents = productsData.map(data => //{
         //return(
           //<Product ID={data.id} Name={data.name} Price={data.price}/>
@@ -72,34 +41,3 @@ function MyPage(){
 }
 
 export default MyPage;
-
-  /*var myInfo = React.createElement('h1', null, 'My Name',
-  React.createElement('p', null, 'blurb'),
-  React.createElement('ul', null, 
-    React.createElement('li', null, 'Iceland'),
-    React.createElement('li', null, 'Peru'),
-    React.createElement('li', null, 'Antarctica'))
-  );
-  /*var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!',
-  React.createElement('ul', null, 
-    React.createElement('li', null, '1'),
-    React.createElement('li', null, '12'),
-    React.createElement('li', null, '123'))
-  ;
-  /*var myInfo = React.createElement('h1', null, 'My Name');
-  React.createElement('p', null, 'blurb',
-    React.createElement('ul', null, 
-      React.createElement('li', null, 'Iceland'),
-      React.createElement('li', null, 'Peru'),
-      React.createElement('li', null, 'Antarctica')
-    )
-  );*/
-// ReactDOM.render(
-//   /*<React.StrictMode>
-//     <App />
-//   </React.StrictMode>,*/
-//   <MyPage />,
-//   //<MemePage />,
-//   //MyInfo(),
-//   //myInfo,
-//   document.getElementById('root'));
